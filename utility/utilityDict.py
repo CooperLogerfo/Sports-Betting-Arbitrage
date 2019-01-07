720f4a3d20e284be31adb53670bf1557d4b0d5f8
CooperLogerfo <32398865+CooperLogerfo@users.noreply.github.com>
2019-01-07 17:43:10 -0500

    Sportsbook dictionaries

---
+++ utility/utilityDict.py
@@ -0,0 +1,43 @@
+"""Sports book dictionaries.
+"""
+
+sportsbookID = {
+	"pinnacle": 238,
+	"fivedimes": 19,
+	"bookmaker": 93,
+	"betonline": 1096,
+	"bovada": 1618,
+	"heritage": 169,
+	"intertops": 180,
+	"youwagereu": 139,
+	"justbet": 1275,
+	"sportsbetting": 289,
+	"GTBets": 1602,
+	"sportsbet": 279,
+	"nitrogensports": 1682,
+	"betphoenix": 1389,
+	"betmania": 1252,
+	"skybook": 274,
+	"mybookie": 1680,
+	"abcislands": 23,
+	"Jazz": 186,
+	"sportsinteraction": 300,
+	"betcris": 118,
+	"SBR": 370,
+	"wagerweb": 414,
+	"thegreek": 227,
+	"bodog": 92,
+	"matchbook": 626, 
+	"100genius": 423,
+	"bet365": 43,
+	"williamhill": 349,
+	"138": 1671
+}
+
+sportsDict = {
+	"MLB": "https://www.sportsbookreview.com/betting-odds/mlb-baseball/money-line/",
+	"NBA": "https://www.sportsbookreview.com/betting-odds/nba-basketball/money-line/",
+	"NCAAB": "https://www.sportsbookreview.com/betting-odds/ncaa-basketball/money-line/",
+	"NCAAF": "",
+	"NHL": "https://www.sportsbookreview.com/betting-odds/nhl-hockey/money-line/"
+}
